e1f8f13c90590ed4f73ed0f8b8ba5905b2b1b9ba
mahan ms <genius20120ms@gmail.com>
2022-06-08 22:56:35 +0430

    bug fi x

--- Front_side/my-app/src/config/config.js
+++ Front_side/my-app/src/config/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  // REACT_APP_REST_API_URL: "/api",
-  REACT_APP_REST_API_URL: "http://localhost:3001",
+  REACT_APP_REST_API_URL: "/api",
+  // REACT_APP_REST_API_URL: "http://localhost:3001", /// DEVELOP
 };
